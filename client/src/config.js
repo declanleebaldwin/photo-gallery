9782087b48b55cb5a6bcabceef9f64c0aa1f5fee
declanbaldwin <declanleebaldwin@gmail.com>
2023-01-07 15:56:21 +0000

    added firebase and config file

---
+++ client/src/config.js
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAJ-SjLjC7ZYT63zl7FZu6ZFOEw6vk4I0g",
+  authDomain: "photo-gallery-e1020.firebaseapp.com",
+  projectId: "photo-gallery-e1020",
+  storageBucket: "photo-gallery-e1020.appspot.com",
+  messagingSenderId: "667664342438",
+  appId: "1:667664342438:web:c49c3e8543337bfc138367"
+}
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { initializeApp } from "firebase/app";
+import { FIREBASE_CONFIG } from "./config"
+
+const firebaseConfig = FIREBASE_CONFIG;
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
