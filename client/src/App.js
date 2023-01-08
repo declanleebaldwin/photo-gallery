58e9bf9242b5863930b5d984e37b085a4ed876ee
declanbaldwin <declanleebaldwin@gmail.com>
2023-01-08 15:56:57 +0000

    created PhotoCard component

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,25 @@ function App() {
   const photos = useCollection({ collectionName: "photos" })
   if (!photos || !photos.length) return <></>
 
+  const PhotoCard = ({ id, imageUrl, title }) => {
+    return <div key={id} className="mb-6"><img className="h-full w-full" src={imageUrl} alt={title} /></div>
+  }
+
   return (
-    <div className="grid grid-cols-gallery content-start gap-x-6 ">
-      <div className="">
-        {photos.filter((_photo, i) => i % 3 === 0).map((photo, i) => {
-          return <div key={photo.id} className="mb-6"><img className="h-full" src={photo.imageUrl} alt={photo.title} /></div>
+    <div className="grid grid-cols-gallery content-start gap-x-6 m-6">
+      <div>
+        {photos.filter((_photo, i) => i % 3 === 0).map((photo) => {
+          return <PhotoCard {...photo} />
         })}
       </div>
-      <div className="">
+      <div>
         {photos.filter((_photo, i) => i % 3 === 1).map((photo, i) => {
-          return <div key={photo.id} className="mb-6"><img className="h-full" src={photo.imageUrl} alt={photo.title} /></div>
+          return <PhotoCard {...photo} />
         })}
       </div>
-      <div className="">
+      <div>
         {photos.filter((_photo, i) => i % 3 === 2).map((photo, i) => {
-          return <div key={photo.id} className="mb-6"><img className="h-full" src={photo.imageUrl} alt={photo.title} /></div>
+          return <PhotoCard {...photo} />
         })}
       </div>
 
